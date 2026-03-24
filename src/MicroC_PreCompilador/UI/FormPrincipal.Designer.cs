92aa9d4f2cadeed3222a0b0fb034efc6f56984e9
Luis Andrés de León velásquez <luisandresdlv@gmail.com>
2026-03-23 23:28:50 -0600

    feat: agregar detección de errores léxicos

--- src/MicroC_PreCompilador/UI/FormPrincipal.Designer.cs
+++ src/MicroC_PreCompilador/UI/FormPrincipal.Designer.cs
@@ -29,17 +29,16 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.txtSalida = new System.Windows.Forms.TextBox();
-            this.panelSuperior = new System.Windows.Forms.Panel();
             this.txtEditor = new System.Windows.Forms.TextBox();
-            this.btnNuevo = new System.Windows.Forms.Button();
-            this.btnAbrir = new System.Windows.Forms.Button();
-            this.btnGuardar = new System.Windows.Forms.Button();
-            this.btnEditar = new System.Windows.Forms.Button();
-            this.btnCompilar = new System.Windows.Forms.Button();
-            this.btnAyuda = new System.Windows.Forms.Button();
+            this.panelSuperior = new System.Windows.Forms.Panel();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.panelLineas = new System.Windows.Forms.Panel();
+            this.btnAyuda = new System.Windows.Forms.Button();
+            this.btnCompilar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.txtSalida = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -56,7 +55,6 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.panelLineas);
             this.splitContainer1.Panel1.Controls.Add(this.txtEditor);
             this.splitContainer1.Panel1.Controls.Add(this.panelSuperior);
             // 
@@ -67,19 +65,19 @@
             this.splitContainer1.SplitterDistance = 266;
             this.splitContainer1.TabIndex = 0;
             // 
-            // txtSalida
+            // txtEditor
             // 
-            this.txtSalida.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(27)))), ((int)(((byte)(30)))));
-            this.txtSalida.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtSalida.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSalida.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(200)))), ((int)(((byte)(150)))));
-            this.txtSalida.Location = new System.Drawing.Point(0, 0);
-            this.txtSalida.Multiline = true;
-            this.txtSalida.Name = "txtSalida";
-            this.txtSalida.ReadOnly = true;
-            this.txtSalida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtSalida.Size = new System.Drawing.Size(800, 180);
-            this.txtSalida.TabIndex = 0;
+            this.txtEditor.AcceptsTab = true;
+            this.txtEditor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.txtEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtEditor.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditor.ForeColor = System.Drawing.Color.Gainsboro;
+            this.txtEditor.Location = new System.Drawing.Point(0, 55);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtEditor.Size = new System.Drawing.Size(800, 211);
+            this.txtEditor.TabIndex = 2;
             // 
             // panelSuperior
             // 
@@ -97,75 +95,32 @@
             this.panelSuperior.Size = new System.Drawing.Size(800, 55);
             this.panelSuperior.TabIndex = 1;
             // 
-            // txtEditor
+            // btnSalir
             // 
-            this.txtEditor.AcceptsTab = true;
-            this.txtEditor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
-            this.txtEditor.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtEditor.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEditor.ForeColor = System.Drawing.Color.Gainsboro;
-            this.txtEditor.Location = new System.Drawing.Point(0, 55);
-            this.txtEditor.Multiline = true;
-            this.txtEditor.Name = "txtEditor";
-            this.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtEditor.Size = new System.Drawing.Size(800, 211);
-            this.txtEditor.TabIndex = 2;
-            this.txtEditor.TextChanged += new System.EventHandler(this.txtEditor_TextChanged);
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.ForeColor = System.Drawing.Color.White;
+            this.btnSalir.Location = new System.Drawing.Point(588, 12);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(80, 35);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
-            // btnNuevo
+            // btnAyuda
             // 
-            this.btnNuevo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.btnNuevo.FlatAppearance.BorderSize = 0;
-            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNuevo.ForeColor = System.Drawing.Color.White;
-            this.btnNuevo.Location = new System.Drawing.Point(12, 12);
-            this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(80, 35);
-            this.btnNuevo.TabIndex = 0;
-            this.btnNuevo.Text = "Nuevo";
-            this.btnNuevo.UseVisualStyleBackColor = false;
-            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
-            // 
-            // btnAbrir
-            // 
-            this.btnAbrir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.btnAbrir.FlatAppearance.BorderSize = 0;
-            this.btnAbrir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAbrir.ForeColor = System.Drawing.Color.White;
-            this.btnAbrir.Location = new System.Drawing.Point(108, 12);
-            this.btnAbrir.Name = "btnAbrir";
-            this.btnAbrir.Size = new System.Drawing.Size(80, 35);
-            this.btnAbrir.TabIndex = 1;
-            this.btnAbrir.Text = "Abrir";
-            this.btnAbrir.UseVisualStyleBackColor = false;
-            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
-            // 
-            // btnGuardar
-            // 
-            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.btnGuardar.FlatAppearance.BorderSize = 0;
-            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGuardar.ForeColor = System.Drawing.Color.White;
-            this.btnGuardar.Location = new System.Drawing.Point(204, 12);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(80, 35);
-            this.btnGuardar.TabIndex = 2;
-            this.btnGuardar.Text = "Guardar";
-            this.btnGuardar.UseVisualStyleBackColor = false;
-            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
-            // 
-            // btnEditar
-            // 
-            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.btnEditar.FlatAppearance.BorderSize = 0;
-            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEditar.ForeColor = System.Drawing.Color.White;
-            this.btnEditar.Location = new System.Drawing.Point(300, 12);
-            this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(80, 35);
-            this.btnEditar.TabIndex = 3;
-            this.btnEditar.Text = "Editar";
-            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnAyuda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAyuda.FlatAppearance.BorderSize = 0;
+            this.btnAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAyuda.ForeColor = System.Drawing.Color.White;
+            this.btnAyuda.Location = new System.Drawing.Point(492, 12);
+            this.btnAyuda.Name = "btnAyuda";
+            this.btnAyuda.Size = new System.Drawing.Size(80, 35);
+            this.btnAyuda.TabIndex = 5;
+            this.btnAyuda.Text = "Ayuda";
+            this.btnAyuda.UseVisualStyleBackColor = false;
             // 
             // btnCompilar
             // 
@@ -181,41 +136,74 @@
             this.btnCompilar.UseVisualStyleBackColor = false;
             this.btnCompilar.Click += new System.EventHandler(this.btnCompilar_Click);
             // 
-            // btnAyuda
+            // btnEditar
             // 
-            this.btnAyuda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.btnAyuda.FlatAppearance.BorderSize = 0;
-            this.btnAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAyuda.ForeColor = System.Drawing.Color.White;
-            this.btnAyuda.Location = new System.Drawing.Point(492, 12);
-            this.btnAyuda.Name = "btnAyuda";
-            this.btnAyuda.Size = new System.Drawing.Size(80, 35);
-            this.btnAyuda.TabIndex = 5;
-            this.btnAyuda.Text = "Ayuda";
-            this.btnAyuda.UseVisualStyleBackColor = false;
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnEditar.FlatAppearance.BorderSize = 0;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(300, 12);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(80, 35);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
             // 
-            // btnSalir
+            // btnGuardar
             // 
-            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
-            this.btnSalir.FlatAppearance.BorderSize = 0;
-            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSalir.ForeColor = System.Drawing.Color.White;
-            this.btnSalir.Location = new System.Drawing.Point(588, 12);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(80, 35);
-            this.btnSalir.TabIndex = 6;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = false;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Location = new System.Drawing.Point(204, 12);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(80, 35);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
-            // panelLineas
+            // btnAbrir
             // 
-            this.panelLineas.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLineas.Location = new System.Drawing.Point(0, 55);
-            this.panelLineas.Name = "panelLineas";
-            this.panelLineas.Size = new System.Drawing.Size(50, 211);
-            this.panelLineas.TabIndex = 3;
-            this.panelLineas.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLineas_Paint);
+            this.btnAbrir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnAbrir.FlatAppearance.BorderSize = 0;
+            this.btnAbrir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbrir.ForeColor = System.Drawing.Color.White;
+            this.btnAbrir.Location = new System.Drawing.Point(108, 12);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(80, 35);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(168)))), ((int)(((byte)(150)))));
+            this.btnNuevo.FlatAppearance.BorderSize = 0;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.ForeColor = System.Drawing.Color.White;
+            this.btnNuevo.Location = new System.Drawing.Point(12, 12);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(80, 35);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(27)))), ((int)(((byte)(30)))));
+            this.txtSalida.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSalida.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalida.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(200)))), ((int)(((byte)(150)))));
+            this.txtSalida.Location = new System.Drawing.Point(0, 0);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.ReadOnly = true;
+            this.txtSalida.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSalida.Size = new System.Drawing.Size(800, 180);
+            this.txtSalida.TabIndex = 0;
             // 
             // FormPrincipal
             // 
@@ -225,10 +213,10 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
             this.Name = "FormPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MicroC - PreCompilador";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
@@ -253,7 +241,6 @@
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnAbrir;
         private System.Windows.Forms.Button btnNuevo;
-        private System.Windows.Forms.Panel panelLineas;
     }
 }
 
--- src/MicroC_PreCompilador/UI/FormPrincipal.cs
+++ src/MicroC_PreCompilador/UI/FormPrincipal.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MicroC_PreCompilador
@@ -16,7 +11,6 @@ namespace MicroC_PreCompilador
         public FormPrincipal()
         {
             InitializeComponent();
-        
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
@@ -27,8 +21,10 @@ namespace MicroC_PreCompilador
         private void btnNuevo_Click(object sender, EventArgs e)
         {
             txtEditor.Clear();
-            txtSalida.Clear(); 
+            txtSalida.Clear();
         }
+
+
 
         private void btnCompilar_Click(object sender, EventArgs e)
         {
@@ -37,12 +33,130 @@ namespace MicroC_PreCompilador
             if (String.IsNullOrWhiteSpace(txtEditor.Text))
             {
                 txtSalida.Text = "Error: No hay código para compilar";
+                return;
+            }
+
+            string codigo = txtEditor.Text;
+            char[] caracteres = codigo.ToCharArray();
+
+            string actual = "";
+            bool enString = false;
+
+            HashSet<string> reservadas = new HashSet<string>()
+    {
+        "int", "float", "if", "else", "while",
+        "for", "return", "main", "void"
+    };
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                char c = caracteres[i];
+
+                // 🔹 Manejo de strings
+                if (c == '"')
+                {
+                    if (enString)
+                    {
+                        actual += c;
+                        txtSalida.AppendText("STRING: " + actual + Environment.NewLine);
+                        actual = "";
+                        enString = false;
+                    }
+                    else
+                    {
+                        if (actual != "")
+                        {
+                            ClasificarToken(actual, reservadas);
+                            actual = "";
+                        }
+
+                        enString = true;
+                        actual += c;
+                    }
+                    continue;
+                }
+
+                if (enString)
+                {
+                    actual += c;
+                    continue;
+                }
+
+                // 🔹 Construcción de tokens
+                if (char.IsLetterOrDigit(c))
+                {
+                    actual += c;
+                }
+                else
+                {
+                    if (actual != "")
+                    {
+                        ClasificarToken(actual, reservadas);
+                        actual = "";
+                    }
+
+                    if (!char.IsWhiteSpace(c))
+                    {
+                        // 🔹 Operadores
+                        if ("+-*/=".Contains(c))
+                        {
+                            if (i + 1 < caracteres.Length && caracteres[i + 1] == '=')
+                            {
+                                txtSalida.AppendText("OPERADOR: " + c + "=" + Environment.NewLine);
+                                i++;
+                            }
+                            else
+                            {
+                                txtSalida.AppendText("OPERADOR: " + c + Environment.NewLine);
+                            }
+                        }
+                        // 🔹 Delimitadores
+                        else if (";(){}".Contains(c))
+                        {
+                            txtSalida.AppendText("DELIMITADOR: " + c + Environment.NewLine);
+                        }
+                        // 🔹 Error léxico
+                        else
+                        {
+                            txtSalida.AppendText("ERROR_LEXICO: " + c + Environment.NewLine);
+                        }
+                    }
+                }
+            }
+
+            // 🔹 Último token
+            if (actual != "")
+            {
+                ClasificarToken(actual, reservadas);
+            }
+
+            // 🔹 Error: string no cerrado
+            if (enString)
+            {
+                txtSalida.AppendText("ERROR_LEXICO: cadena no cerrada" + Environment.NewLine);
+            }
+        }
+
+        private void ClasificarToken(string token, HashSet<string> reservadas)
+        {
+            if (token.All(char.IsDigit))
+            {
+                txtSalida.AppendText("NUMERO: " + token + Environment.NewLine);
+            }
+            else if (char.IsDigit(token[0]) && token.Any(char.IsLetter))
+            {
+                txtSalida.AppendText("ERROR_LEXICO: identificador invalido -> " + token + Environment.NewLine);
+            }
+            else if (reservadas.Contains(token))
+            {
+                txtSalida.AppendText("PALABRA_RESERVADA: " + token + Environment.NewLine);
             }
             else
             {
-                txtSalida.Text = "Compilación exitosa (simulación)";
+                txtSalida.AppendText("IDENTIFICADOR: " + token + Environment.NewLine);
             }
         }
+
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
@@ -53,7 +167,6 @@ namespace MicroC_PreCompilador
             {
                 File.WriteAllText(guardar.FileName, txtEditor.Text);
             }
-
         }
 
         private void btnAbrir_Click(object sender, EventArgs e)
@@ -65,43 +178,6 @@ namespace MicroC_PreCompilador
             {
                 txtEditor.Text = File.ReadAllText(abrir.FileName);
             }
-
-        }
-
-        private void panelLineas_Paint(object sender, PaintEventArgs e)
-        {
-            int primeraLinea = txtEditor.GetLineFromCharIndex(txtEditor.GetCharIndexFromPosition(new Point(0, 0)));
-            int ultimaLinea = txtEditor.GetLineFromCharIndex(txtEditor.GetCharIndexFromPosition(new Point(0, txtEditor.Height)));
-
-            for (int i = primeraLinea; i <= ultimaLinea + 1; i++)
-            {
-                int y = txtEditor.GetPositionFromCharIndex(txtEditor.GetFirstCharIndexFromLine(i)).Y;
-                e.Graphics.DrawString(
-                    (i + 1).ToString(),
-                    new Font("Consolas", 10),
-                    Brushes.Gray,
-                    new PointF(5, y)
-                );
-            }
-        }
-
-        private void txtEditor_TextChanged(object sender, EventArgs e)
-        {
-            panelLineas.Invalidate();
-        }
-
-
-        protected override void WndProc(ref Message m)
-        {
-            base.WndProc(ref m);
-
-            const int WM_VSCROLL = 0x115;
-            const int WM_MOUSEWHEEL = 0x20A;
-
-            if (m.Msg == WM_VSCROLL || m.Msg == WM_MOUSEWHEEL)
-            {
-                panelLineas.Invalidate();
-            }
         }
     }
- }
+}
